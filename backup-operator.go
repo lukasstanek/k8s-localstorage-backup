6a8e673f091fcfcf85d72b7e26f65d71b414c77c
Lukas Stanek <mail@stanek.xyz>
2023-06-21 16:46:58 +0200

    add test backup

--- backup-operator.go
+++ backup-operator.go
@@ -37,6 +37,66 @@ func main() {
 	for _, pv := range pvList.Items {
 		if pv.Spec.StorageClassName == specificStorageClass {
 			fmt.Printf("PersistentVolume: %s, Claim: %s/%s, Path: %s\n", pv.Name, pv.Spec.ClaimRef.Namespace, pv.Spec.ClaimRef.Name, pv.Spec.HostPath.Path)
+			if pv.Spec.Name == "pvc-01100b40-61f9-4166-a904-c39437696f39" {
+				fmt.Printf("Backing up...")
+				Tar(fmt.Printf("/host%s", pv.Spec.HostPath.Path), fmt.Printf("/backup/%s", time.Now().Format(time.RFC3339)))
+				fmt.Printf("Finished")
+			}
 		}
+
 	}
+}
+
+func Tar(source, target string) error {
+	filename := filepath.Base(source)
+	target = filepath.Join(target, fmt.Sprintf("%s.tar", filename))
+	tarfile, err := os.Create(target)
+	if err != nil {
+		return err
+	}
+	defer tarfile.Close()
+
+	tarball := tar.NewWriter(tarfile)
+	defer tarball.Close()
+
+	info, err := os.Stat(source)
+	if err != nil {
+		return nil
+	}
+
+	var baseDir string
+	if info.IsDir() {
+		baseDir = filepath.Base(source)
+	}
+
+	return filepath.Walk(source,
+		func(path string, info os.FileInfo, err error) error {
+			if err != nil {
+				return err
+			}
+			header, err := tar.FileInfoHeader(info, info.Name())
+			if err != nil {
+				return err
+			}
+
+			if baseDir != "" {
+				header.Name = filepath.Join(baseDir, strings.TrimPrefix(path, source))
+			}
+
+			if err := tarball.WriteHeader(header); err != nil {
+				return err
+			}
+
+			if info.IsDir() {
+				return nil
+			}
+
+			file, err := os.Open(path)
+			if err != nil {
+				return err
+			}
+			defer file.Close()
+			_, err = io.Copy(tarball, file)
+			return err
+		})
 }
